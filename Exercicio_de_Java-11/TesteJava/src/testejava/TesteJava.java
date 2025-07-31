7d58410016e8c8a067d7b990763a9cfcc6841f7e
Vitor Souza <104541182+VitorSouza01@users.noreply.github.com>
2025-07-31 00:27:01 -0300

    eighth commit
    eighth commit

---
+++ Exercicio_de_Java-11/TesteJava/src/testejava/TesteJava.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testejava;
+
+/**
+ *
+ * @author Souza
+ */
+public class TesteJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c = 'c';
+        int i = 10;
+        double d = 10;
+        long l = 1;
+        String s = "Hello";
+        
+        // ERRO c = c + i;
+        // CORRETA s += i;
+        // ERRO i += s;
+        // ERRO c += s;
+        // CORRETO i += l;
+    }
+    
+}
