9e1e683cd903c82b4b74236bc3c102aaf035879f
Vitor Souza <104541182+VitorSouza01@users.noreply.github.com>
2025-11-25 00:37:31 -0300

    tenth commit
    tenth commit

---
+++ Aula_13-Estruturas_de_Repetição-Parte_3/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Souza
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=0; cc<=3; cc++) {
+            System.out.println("Cambalhota");
+        }
+        for (int teste=1; teste<=100; teste+=10) {
+            System.out.println(teste);
+        }
+        /*
+   
+        int cc = 0;
+        while (cc<4) {
+            System.out.println("Cambalhota");
+            cc++;
+        }
+        
+        */
+    }
+    
+}
