0146136d0f8d797bb1952a9586abfc067d1d8b7f
Vitor Souza <104541182+VitorSouza01@users.noreply.github.com>
2025-12-02 00:53:20 -0300

    twelfth commit
    twelfth commit

---
+++ Exercicio_de_Java-15/Funcao01/src/funcao01/Funcao01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funcao01;
+
+/**
+ *
+ * @author Souza
+ */
+public class Funcao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fatorial_1 f = new Fatorial_1();
+        f.setValor(5);
+        System.out.println(f.getFormula());
+        System.out.println(f.getFatorial());
+    }
+    
+}
